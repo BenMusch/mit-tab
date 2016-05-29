a5be45cff8b653184f6c45b0a6fdc646efaa4f6e
benmusch <benmuschol@gmail.com>
2016-05-29 14:52:05 -0400

    fix merge conflicts

--- mittab/wsgi.py
+++ mittab/wsgi.py
@@ -12,10 +12,5 @@ sys.path.append('/var/www/tab')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mittab.settings")
 
 from django.core.wsgi import get_wsgi_application
-<<<<<<< HEAD
 from dj_static import Cling, MediaCling
 application = get_wsgi_application()
-=======
-from dj_static import Cling
-application = Cling(get_wsgi_application())
->>>>>>> 7f6fe82f6f1d6c5e18faf566f0baab8344023560
